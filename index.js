6ffe2e399fcc4713803cef885c73e1b96c37999d
artchamill <arthurchamill@hotmail.com>
2018-08-08 15:05:36 -0400

    Game logic and styling
    index.html
    -Add sign in form
    -Add change password form
    -Add sign out button
    
    indes.js
    -Set game conditions
    -Set winning combinations

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ require('./assets/scripts/index.js')
 
 // styles
 require('./assets/styles/index.scss')
+
+// game conditions
+let players = 2
+let player1Wins = 0
+let player2Wins = 0
+
+// winning box combinations
+const diagonalboxes = [1, 5, 7] [3, 5, 9]
+const horizontalboxes = [1, 2, 3] [4, 5, 6] [7, 8 , 9]
+const verticalboxes = [1, 4, 7] [2, 5, 8] [3, 6, 9]

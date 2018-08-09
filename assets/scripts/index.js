0de1510eb438a4706bde80c888b216a0196f396f
artchamill <arthurchamill@hotmail.com>
2018-08-09 16:58:14 -0400

    Gameboard logic and gameboard html
    -added gameboard win if then statement
    -adjusted gameboard class name

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,42 +5,56 @@ console.log("Up and running!");
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-$(() => {
-  // game board
-  const board = ['box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box7', 'box8', 'box9']
-})
-// game conditions
-const players = 2
-let player1Wins = 0
-let player2Wins = 0
-let moves = 0
 
-// players
-const player1 = 'x'
-const player2 = 'o'
-
-// player moves
-const playerMoves = {
-  'x': [],
-  'o': []
-}
-
-// winning box combinations array
-const winningCombinations = [
-// diagonalWins
-  [0, 4, 6], [2, 4, 8],
-// horizontalWins
-  [0, 1, 2], [3, 4, 5], [6, 7, 8],
-// verticalWins
-  [0, 3, 6], [1, 4, 7], [2, 5, 8]
+// game board
+const gameBoard = [
+  ['X ', ' X', 'X '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
 ]
 
+if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
+  (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') ||
+  (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') ||
+  (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') ||
+  (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') ||
+  (gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X') ||
+  (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X') ||
+  (gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') ||
+
+// game conditions
+// const players = 2
+// let player1Wins = 0
+// let player2Wins = 0
+// let moves = 0
+
+// players
+// const player1 = 'x'
+// const player2 = 'o'
+
+// player moves
+// const playerMoves = {
+//   'x': [],
+//   'o': []
+// }
+
+// winning box combinations array
+// const winningCombinations = [
+// // horizontalWins
+//   [0, 1, 2], [3, 4, 5], [6, 7, 8],
+// // verticalWins
+//   [0, 3, 6], [1, 4, 7], [2, 5, 8]
+//   // diagonalWins
+//     [0, 4, 6], [2, 4, 8],
+// ]
+
 // check for win function
-const checkForWin = function () {
-  winningCombinations.forEach(function (element) {
-    console.log(element)
-  // insert winningCombinations.every here
-  })
+// const checkForWin = function () {
+//   winningCombinations.forEach(function (element) {
+//     console.log(element)
+//   // insert winningCombinations.every here
+//   })
+  // console.log(checkForWin())
   // if (winningCombinations === [0, 4, 6], [2, 4, 8],
   //   [0, 1, 2], [3, 4, 5], [6, 7, 8],
   //   [0, 3, 6], [1, 4, 7], [2, 5, 8]) {
@@ -48,12 +62,18 @@ const checkForWin = function () {
   // } else {
   //   alert('Sorry. Try Again')
   // }
-}
-//click handler for boxes
+// }
 
-$('.gameboard').on('click', '.box', function (event) {
-  const $id = $(event.currentTarget)
-  $box.addClass('xmove')
-})
+// click handler for boxes
+// $('.gameboard').on('click', '.box', function (event) {
+//   const $id = $(event.currentTarget)
+//   $box.addClass('xmove')
+// })
 
-console.log(checkForWin())
+// $('.gameboard').on('click', '.box', function () {
+//   alert('X')
+// })
+
+// $(() => {
+//
+// })
